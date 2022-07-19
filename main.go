e486d55f3a878f0b18952e84c7ec7f71f075827e
Shion Ichikawa <shion1305@gmail.com>
2022-07-19 13:52:51 +0900

    ✏️ typo "BOok"->"Book"

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type todo struct {
 
 var todos = []todo{
 	{ID: "1", Item: "Clean Room", Completed: false},
-	{ID: "2", Item: "Read BOok", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
 	{ID: "3", Item: "Record Video", Completed: false},
 }
 
